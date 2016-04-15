18ab525d17a8037b3a8105c7e49efaf0756cb18a
David Czeck <david@ibiblio.org>
2016-04-15 16:57:19 -0400

    Support for cancel on background

--- app/demo-app.component.ts
+++ app/demo-app.component.ts
@@ -12,6 +12,7 @@ import {SimpleModal, SimpleModalType} from './simple-modal';
 			<label class="form-label">Settings:</label>
 			<label><input type="checkbox" [(ngModel)]="customize">Customize</label>
 			<label><input type="checkbox" [(ngModel)]="useConfirm">Confirm button</label>
+			<label><input type="checkbox" [(ngModel)]="modal.blocking">Blocking</label>
 			<label><input type="checkbox" [(ngModel)]="showResult">Show result</label>
 			<div>
 				<label class="form-label">Modal type:</label>
@@ -21,18 +22,20 @@ import {SimpleModal, SimpleModalType} from './simple-modal';
 		<fieldset *ngIf="customize">
 			<p><label class="form-label">Title:</label><input type="text" [(ngModel)]="modal.title"></p>
 			<p><label class="form-label">Message:</label><input type="text" [(ngModel)]="modal.message"></p>
+			<p *ngIf="useConfirm"><label class="form-label">Confirm button:</label><input type="text" [(ngModel)]="modal.confirmBtn"></p>
 			<p><label class="form-label">Cancel button:</label><input type="text" [(ngModel)]="modal.cancelBtn"></p>
 		
-			<p *ngIf="useConfirm"><label class="form-label">Confirm button:</label><input type="text" [(ngModel)]="modal.confirmBtn"></p>
 			<p><label class="form-label">Height:</label><input type="number" [(ngModel)]="modal.height" min="150" max="400"></p>
 			<p><label class="form-label">Width:</label><input type="number" [(ngModel)]="modal.width" min="250" max="640"></p>
 
 		</fieldset>
 	</form>
-	<p *ngIf="showResult"><label class="form-label">Result:</label>{{result}}&nbsp;</p>
-	<div style="margin:25px 10px;"><button (click)="showModal()">Show Modal</button></div>
-</div>
-`
+	<p *ngIf="showResult"><label class="form-label">Last result:</label>{{result}}&nbsp;</p>
+	<div style="margin:25px 10px;">
+		<button (click)="showModal($event)">Show Modal</button><label>
+		<input type="checkbox" [(ngModel)]="demoCascade">Cascade?</label>
+	</div>
+</div>`
 })
 
 export class DemoAppComponent implements OnInit {
@@ -42,6 +45,7 @@ export class DemoAppComponent implements OnInit {
 	private showResult:boolean = false;
 	private customize:boolean = false;
 	private useConfirm:boolean = false;
+	private demoCascade:boolean = false;
 	private result:string;
 
 	constructor(private modal:SimpleModal) {
@@ -55,39 +59,36 @@ export class DemoAppComponent implements OnInit {
 		}
 	}
 
-	private radio(index:any) {
+	radio(index:any) {
 		this.mt = index;
 
 		switch (this.mt) {
 		case SimpleModalType.Info:
 			this.modal.title = 'Information';
 			this.modal.message = 'This is a simple informational message.';
-			this.modal.cancelBtn = 'Ok';
-			this.modal.confirmBtn = null;
 			this.modal.type = SimpleModalType.Info;
 			break;
 		case SimpleModalType.Warning:
 			this.modal.title = 'Warning';
 			this.modal.message = 'This is a simple warning message.';
-			this.modal.cancelBtn = 'Ok';
-			this.modal.confirmBtn = null;
 			this.modal.type = SimpleModalType.Warning;
 			break;
 		case SimpleModalType.Critical:
 			this.modal.title = 'Critical';
 			this.modal.message = 'This is a simple critical message.';
-			this.modal.cancelBtn = 'Ok';
-			this.modal.confirmBtn = null;
 			this.modal.type = SimpleModalType.Critical;
 			break;
 		default:
 			this.modal.title = 'Default title';
 			this.modal.message = 'Default message.';
-			this.modal.cancelBtn = 'Ok';
-			this.modal.confirmBtn = null;
 			this.modal.type = SimpleModalType.Default;
 			break;
 		}
+		this.modal.cancelBtn = 'OK';
+		this.modal.confirmBtn = null;
+		this.modal.blocking = true;
+		this.modal.width = 250;
+		this.modal.height = 150;
 		this.confirmText();
 	}
 
@@ -99,15 +100,56 @@ export class DemoAppComponent implements OnInit {
 		}
 	}
 
-	showModal() {
+	cancelText() {
+		if (this.modal.blocking && this.modal.cancelBtn.length === 0) {
+			this.modal.cancelBtn = 'OK';
+		}
+	}
+
+
+	private cascade() {
+		let w:number = this.modal.width;
+		let h:number = this.modal.height;
+		let t:string = this.modal.title;
+
+		if (this.demoCascade) {
+			this.modal.width = w + 150;
+			this.modal.height = h + 150;
+			this.modal.title = 'Cascade 1 ' + t;
+
+			if (!this.showResult) {
+				this.modal.show();
+			} else {
+				this.modal.show().then( (res) => this.result = res);
+			}
+
+			this.modal.width = w;
+			this.modal.height = h;
+			this.modal.title = 'Cascade 2 ' + t;
+
+			if (!this.showResult) {
+				this.modal.show();
+			} else {
+				this.modal.show().then( (res) => this.result = res);
+			}
+
+			this.modal.title = t;
+		}
+	}
+
+	showModal(e) {
+		e.target.blur();
+
 		this.result = null;
 		this.confirmText();
+		this.cancelText();
 
 		if (!this.showResult) {
 			this.modal.show();
 		} else {
 			this.modal.show().then( (res) => this.result = res);
 		}
+		this.cascade();
 	}
 
 }
--- app/simple-modal.ts
+++ app/simple-modal.ts
@@ -1,4 +1,5 @@
 import {Component, ComponentRef, DynamicComponentLoader, ApplicationRef, ElementRef, Injectable} from 'angular2/core';
+import {NgClass, NgStyle} from 'angular2/common';
 import {PromiseWrapper} from 'angular2/src/facade/async';
 
 
@@ -12,27 +13,33 @@ export enum SimpleModalType {
 @Injectable()
 export class SimpleModal {
 
+	private zindex = 99;
+
 	title:string = 'Default title';
 	message:string = 'Default message';
 	type:SimpleModalType = SimpleModalType.Default;
+	blocking:boolean = true;
 	confirmBtn:string = null;
-	cancelBtn:string = 'Ok';
+	cancelBtn:string = 'OK';
 	width:number = 250;
 	height:number = 150;
 
-	template:string =
-`<div class="modal" [ngStyle]="{'width':width, 'height':height}">
-	<img *ngIf="icon" class="modal-icon" [src]="icon" alt="" title="" />
-	<h2 class="modal-title">{{title}}</h2>
-	<div class="modal-message" [ngStyle]="{'height':msgHeight}">{{message}}</div>
-	<div class="modal-buttonbar">
-		<button *ngIf="confirmBtn" (click)="affirm()">{{confirmBtn}}</button>
-		<button (click)="cancel()" >{{cancelBtn}}</button>
-	</div>
-</div>
-`;
+	template:string =`
+		<img *ngIf="icon" class="modal-icon" [src]="icon" alt="" title="" />
+		<h2 class="modal-title">{{title}}</h2>
+		<div class="modal-message">{{message}}</div>
+		<div class="modal-buttonbar">
+			<button *ngIf="confirmBtn" (click)="confirm()">{{confirmBtn}}</button>
+			<button *ngIf="cancelBtn" (click)="cancel()" >{{cancelBtn}}</button>
+		</div>`;
 
 	constructor(private dcl:DynamicComponentLoader, private app:ApplicationRef) {
+	}
+
+	// Add/subtract z-index for cascading modals from same (this) provider.
+	private strata(z:number) : number {
+		this.zindex += z;
+		return this.zindex;
 	}
 
 	toComponent() : Function {
@@ -40,11 +47,21 @@ export class SimpleModal {
 		let message:string = this.message;
 		let width:string = this.width + 'px';
 		let height:string = this.height + 'px';
-		let msgHeight:string = this.height - 95 + 'px';
 		let confirmBtn:string = this.confirmBtn;
 		let cancelBtn:string = this.cancelBtn;
-		let template:string = this.template + '<div class="modal-background"></div>';
 		let icon:string = null;
+		let provider:SimpleModal = this;
+		let bz:number = this.strata(1);    // Background z-index
+		let z:number = bz + 1;              // Modal z-index
+		let template:string;
+
+		if (this.blocking) {
+			template = `<div class="modal" [ngStyle]="{'width':'` + width + `', 'height':'` + height + `', 'z-index':` + z + '}">' +
+					this.template + `</div><div [ngStyle]="{'z-index':` + bz + '}" class="modal-background">';
+		} else {
+			template = `<div class="modal" [ngStyle]="{'width':'` + width + `', 'height':'` + height + `', 'z-index':` + z + '}">' +
+					this.template + `</div><div [ngStyle]="{'z-index':` + bz + '}" class="modal-background" (click)="cancel()">';
+		}
 
 		switch (this.type) {
 			case SimpleModalType.Info:
@@ -63,6 +80,7 @@ export class SimpleModal {
 		// Note: Do NOT use styleUrls, because they'll keep getting added to the DOM.
 		@Component({
 			selector: 'modal',
+			directives: [ NgClass, NgStyle ],
 			template: template
 		})
 		class Modal {
@@ -71,22 +89,21 @@ export class SimpleModal {
 			/* tslint:disable:no-unused-variable */
 			private title:string = title;
 			private message:string = message;
-			private width:string = width;
-			private height:string = height;
-			private msgHeight:string = msgHeight;
 			private icon:string = icon;
+			private provider:SimpleModal = provider;
 			/* tslint:enable:no-unused-variable */
 			private confirmBtn:string = confirmBtn;
 			private cancelBtn:string = cancelBtn;
 			private result:any;
 
-
-			affirm() {
+			confirm() {
+				provider.strata(-1);
 				this.cref.dispose();
 				this.result.resolve(this.confirmBtn);
 			}
 
 			cancel() {
+				provider.strata(-1);
 				this.cref.dispose();
 
 				// By rejecting, the show must catch the error. So by resolving,
