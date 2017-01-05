72a926cbd21e4b7b45665c34f8f99352ce5eebe6
David Czeck <david@ibiblio.org>
2017-01-04 23:34:08 -0800

    Update to 2.4.0

--- demo/systemjs.config.js
+++ demo/systemjs.config.js
@@ -1,11 +1,11 @@
 (function(global) {
 
-  var ngVer = '@2.3.0';
+  var ngVer = '@2.4.0';
 
   // map tells the System loader where to look for things
   var map = {
     'app':  'app', // 'dist',
-    'rxjs': 'https://unpkg.com/rxjs@5.0.0-rc.4',
+    'rxjs': 'https://unpkg.com/rxjs@5.0.1',
     '@angular': 'https://unpkg.com/@angular'
   };
 
